31f019b2d3a0cd1cf3525f7993f3a4a7dc46ad06
coolbrother <coolbrother@gmail.com>
2024-02-17 17:11:30 -0500

    Position Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoServoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoServoConstants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.constants;
 public class AutoServoConstants {
     public static final double[] LEServoPositions = {0.08, 0.3, .64, 0.84};
     public static final double[] REServoPositions = {0.08, 0.3, .64, 0.84};
-    public static final double[] WServoPositions = {0.93, .3, .5};
+    public static final double[] WServoPositions = {0.93, .35, .5};
     public static final double[] ClawLPositions = {0.1, 0.65, 0.82};
     public static final double[] ClawRPositions = {0, 0.65, 0.82};
     public static final double[] FingerFPositions = {0.5, 0};
