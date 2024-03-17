dafcae09efceb34c3d4cfb913ea58d7b0784314a
9848 <9848@student.clearviewregional.edu>
2024-03-17 08:36:22 -0400

    Fixed Arm Crashing Into Spools

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/TeleopServoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/TeleopServoConstants.java
@@ -9,7 +9,7 @@ public class TeleopServoConstants {
 
 
     public static final double[]  LEServoPositions = {0.15, 0.3, .8, 0.2}; /*0.80*/
-    public static final double[] REServoPositions = {0.15, 0.3, .8, 0.2};
+    public static final double[] REServoPositions = {0.15, 0.3, .8, 0.18};
     public static final double[] WServoPositions = {.95, .36, .56, 0.73};
     public static final double[] ClawLPositions = {0, 0.65, 0.82};
     public static final double[] ClawRPositions = {0, 0.65, 0.82};
